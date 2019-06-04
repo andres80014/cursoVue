a8f01ec42103a1d5fb9e07a2cbd83c81bdfa3aa0
Andres Vaca <jose.vaca@decameron.com>
2019-06-04 16:54:32 -0500

    Ajustes!

--- comunicacion/hijo.js
+++ comunicacion/hijo.js
@@ -1,6 +1,15 @@
 Vue.component('hijo',{
     template:`<div class='p-5 bg-success'>
     <h4>Componente Hijo => {{ numero }}</h4>
+    <h4>Enviar al padre nombre  => {{ nombre }}</h4>
+    
     </div>`,
-    props:['numero']
+    props:['numero'],
+    data(){
+        return{
+            nombre:'Jose '
+        }
+    },mounted() {
+        this.$emit('nombreHijo',this.nombre);
+    },
 })
--- comunicacion/padre.js
+++ comunicacion/padre.js
@@ -2,14 +2,18 @@ Vue.component('padre',{
     template: `
     <div class='p-5 bg-dark text-white'>
         <h2>Componente padre : {{ numeroPadre }}</h2>
-        <hijo :numero="numeroPadre"></hijo>
+        <hijo :numero="numeroPadre" @nombreHijo="recibeHijo = $event "></hijo>
+        {{ recibeHijo }}
+
         <button @click="numeroPadre++">+</button>
     </div> 
     `,
     data(){
         return{
-            numeroPadre : 10
+            numeroPadre : 10,
+            recibeHijo :''
         } 
     }
+
     
 })
